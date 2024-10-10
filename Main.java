8d71f0e75569f6587604c765063c85083b59404a
Andry925 <andry925@ukr.net>
2024-10-10 18:25:36 +0300

    feat: add error handling

--- Main.java
+++ Main.java
@@ -1,32 +1,49 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class MatrixOperation {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter The Number Of Matrix Rows");
-        int matrixRow = scan.nextInt();
+        try {
+            System.out.println("Enter the number of matrix rows");
+            int matrixRow = scan.nextInt();
+            if (matrixRow <= 0) {
+                throw new IllegalArgumentException("Number of rows must be a positive number.");
+            }
 
-        System.out.println("Enter The Number Of Matrix Columns");
-        int matrixCol = scan.nextInt();
+            System.out.println("Enter the number of matrix columns");
+            int matrixCol = scan.nextInt();
+            if (matrixCol <= 0) {
+                throw new IllegalArgumentException("Number of columns must be a positive number.");
+            }
 
-        System.out.println("Enter the constant to multiply matrix by");
-        float matrixMultiply = scan.nextFloat();
+            System.out.println("Enter the constant to multiply matrix by");
+            float matrixMultiply = scan.nextFloat();
 
-        float[][] matrix = new float[matrixRow][matrixCol];
-        enterMatrixData(scan, matrix, matrixRow, matrixCol, matrixMultiply);
+            float[][] matrix = new float[matrixRow][matrixCol];
+            enterMatrixData(scan, matrix, matrixRow, matrixCol, matrixMultiply);
 
-        printMatrix(matrix, matrixRow, matrixCol);
-        float average = findAverageSumMatrix(matrix, matrixRow, matrixCol);
-        System.out.println("The average sum of matrix elements is: " + average);
+            printMatrix(matrix, matrixRow, matrixCol);
+            float average = findAverageSumMatrix(matrix, matrixRow, matrixCol);
+            System.out.println("The average sum of matrix elements is: " + average);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter numeric values.");
+        }
+        catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+        } finally {
+            scan.close();
+        }
     }
 
     public static void enterMatrixData(Scanner scan, float[][] matrix, int matrixRow, int matrixCol, float matrixMultiply) {
         System.out.println("Enter Matrix Data");
-
         for (int i = 0; i < matrixRow; i++) {
             for (int j = 0; j < matrixCol; j++) {
+                System.out.println("Enter element");
                 matrix[i][j] = scan.nextFloat();
                 matrix[i][j] *= matrixMultiply;
             }
@@ -46,12 +63,12 @@ class MatrixOperation {
     public static float findAverageSumMatrix(float[][] matrix, int matrixRow, int matrixCol) {
         int elementCount = matrixRow * matrixCol;
         float matrixSum = 0;
-
         for (int i = 0; i < matrixRow; i++) {
             for (int j = 0; j < matrixCol; j++) {
                 matrixSum += matrix[i][j];
             }
         }
         return matrixSum / elementCount;
+
     }
 }
